6d33f3ff772d4204e955e5703c99fc71800f9950
Smirk <manav.maj@outlook.com>
2022-10-23 10:06:15 -0400

    Newest Changes

--- src/modules/main/server/events/villager2.ts
+++ src/modules/main/server/events/villager2.ts
@@ -1,6 +1,7 @@
 import { RpgEvent, EventData, RpgPlayer} from '@rpgjs/server'
 import defaultGui from '@rpgjs/default-gui' 
 import fetch from 'node-fetch';
+const FormData = require('form-data');
 
 @EventData({
     name: 'EV-2', 
@@ -14,6 +15,9 @@ export class VillagerEvent2 extends RpgEvent {
         this.setGraphic('female132')
     }
     async onAction(player: RpgPlayer) {
+
+        const form = new FormData();
+        form.append('username', "smirk");
         
         const response = await fetch('https://webhook.site/01d3bf18-ee12-44dc-ba16-53de2b3a11bc/get')
         const data = await response.json()
